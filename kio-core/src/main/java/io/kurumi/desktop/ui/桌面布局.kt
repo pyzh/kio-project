2b378cbf52d1e493ef8c0a5972ef8cc7d869d2a9
MikaGuraN <1069952275@qq.com>
2018-08-10 13:03:38 +0800

    提交

--- kio-core/src/main/java/io/kurumi/desktop/ui/桌面布局.kt
+++ kio-core/src/main/java/io/kurumi/desktop/ui/桌面布局.kt
@@ -9,7 +9,7 @@ import javafx.scene.Node
 import javafx.scene.layout.Pane
 import java.util.*
 
-open class 桌面布局 (override val 视图: 布局, override val 内容 : Pane) : 桌面视图(视图,内容), 基本布局 ,MutableCollection<视图> {
+open class 桌面布局 (override val 内容 : Pane) : 桌面视图(内容), 基本布局 {
 
     private val 子视图 = LinkedList<视图>()
 
@@ -66,47 +66,4 @@ open class 桌面布局 (override val 视图: 布局, override val 内容 : Pane
         return _所有
     }
 
-    override val size: Int
-        get() = 子视图.size
-
-    override fun contains(element: 视图): Boolean {
-        return 子视图.contains(element)
-    }
-
-    override fun containsAll(elements: Collection<视图>): Boolean {
-        return 子视图.containsAll(elements)
-    }
-
-    override fun isEmpty(): Boolean {
-        return 子视图.isEmpty()
-    }
-
-    override fun add(element: 视图): Boolean {
-        加入子视图(element)
-        return true
-    }
-
-    override fun addAll(elements: Collection<视图>): Boolean {
-        加入子视图(elements.toTypedArray())
-    }
-
-    override fun clear() {
-        TODO()
-    }
-
-    override fun iterator(): MutableIterator<视图> {
-        TODO()
-    }
-
-    override fun remove(element: 视图): Boolean {
-        TODO()
-    }
-
-    override fun removeAll(elements: Collection<视图>): Boolean {
-        TODO()
-    }
-
-    override fun retainAll(elements: Collection<视图>): Boolean {
-        TODO()
-    }
 }
--- kio-core/src/main/java/io/kurumi/ui/abs/基本布局.kt
+++ kio-core/src/main/java/io/kurumi/ui/abs/基本布局.kt
@@ -4,6 +4,7 @@ import io.kurumi.ui.widget.视图
 
 interface 基本布局 : 基本视图 {
 
+
     fun 加入子视图(vararg _视图: 视图)
 
     fun 取子视图(_键值: Int): 视图
