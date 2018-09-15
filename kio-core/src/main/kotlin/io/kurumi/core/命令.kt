8715f6480f2452c913b92ea0c2b397cefbc67fce
MikaGuraN <1069952275@qq.com>
2018-09-15 10:46:11 +0800

    提交

--- kio-core/src/main/kotlin/io/kurumi/core/命令.kt
+++ kio-core/src/main/kotlin/io/kurumi/core/命令.kt
@@ -19,6 +19,6 @@ package io.kurumi.core
 
 expect object 命令 {
 
-    fun 执行(_命令: String, _回调: (_成功: Boolean, _结果: String) -> Unit = { _成功, _结果 -> })
+    fun 执行(_命令: String, _回调: (_成功: Boolean, _结果: String) -> Unit = { _,_ -> })
 
 }
